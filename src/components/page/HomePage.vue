97061502af7b806d70e011c8dc1fd8153e795932
rifqithomirt <rifqi.arti.rt@gmail.com>
2021-10-09 09:43:54 +0700

    Update Bootstrap

--- src/components/page/HomePage.vue
+++ src/components/page/HomePage.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="container-home">Home</div>
+  <div class="container-home container">
+    <p1>Home</p1>
+    <div class="btn btn-primary">
+      Home  
+    </div>
+  </div>
 </template>
 
 <script>
